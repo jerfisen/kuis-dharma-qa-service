8c74f1e52af20402c61fceb6299f801e086dd961
lionmanbloodsucker <lionmanbloodsucker@yahoo.com>
2019-05-22 00:59:46 +0700

    add page info

---
+++ src/common/page.info.ts
@@ -0,0 +1,28 @@
+import { ObjectType, Field, Int, ID, ArgsType } from 'type-graphql';
+import { Min, Max } from 'class-validator';
+@ObjectType()
+export class PageInfo {
+    @Field( type => Int )
+    total_pages: number;
+
+    @Field( type => Int )
+    total_result: number;
+
+    @Field( type => Int )
+    current_page: number;
+
+    @Field( type => Int )
+    per_page: number;
+}
+
+@ArgsType()
+export class ArgsPageInfo {
+    @Field( type => Int )
+    @Min(1)
+    @Max(100)
+    per_page: number = 25;
+
+    @Field( type => Int )
+    @Min(1)
+    current_page: number = 1;
+}
