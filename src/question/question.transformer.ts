bc71209ecbce5e4f2b05394b33a89edd28148a8e
lionmanbloodsucker <lionmanbloodsucker@yahoo.com>
2019-06-06 14:06:09 +0700

    inject topic transformer

--- src/question/question.transformer.ts
+++ src/question/question.transformer.ts
@@ -2,15 +2,21 @@ import { Injectable } from "@nestjs/common";
 import { Answer, Question } from './question.dto';
 import { AnswerEntity, QuestionEntity } from 'kuis-dharma-database';
 import { plainToClass } from 'class-transformer';
+import { TopicTransformer } from '../topic/topic.transformer';
 
 @Injectable()
 export class QuestionTransformer {
+
+    constructor(
+        private readonly topic_transformer: TopicTransformer,
+    ){}
 
     toQuestion( entity: QuestionEntity ): Question {
         return plainToClass( Question, {
             id: entity.id,
             text_content: entity.text_content,
             media_content: entity.media_content,
+            topics: entity.topics.map( ( topic ) => this.topic_transformer.toTopic(topic) ),
         } as Question );
     }
 
