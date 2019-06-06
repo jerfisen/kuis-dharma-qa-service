67fbfe48961085719471eccda8af942dbc9eb526
lionmanbloodsucker <lionmanbloodsucker@yahoo.com>
2019-06-06 14:04:33 +0700

    add question module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import DatabaseModule from './common/database.module';
 import { TopicModule } from './topic/topic.module';
+import { QuestionModule } from './question/question.module';
 
 @Module({
 	imports: [
 		DatabaseModule,
 		TopicModule,
+		QuestionModule,
 		GraphQLModule.forRoot({
 			installSubscriptionHandlers: true,
 			autoSchemaFile: 'schema.gql',
