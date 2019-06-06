efdb0c56c20fa7622b42f32e9e783db25ed95790
lionmanbloodsucker <lionmanbloodsucker@yahoo.com>
2019-06-06 14:04:15 +0700

    now only support env port

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  if ( process.env.PORT ) await app.listen( process.env.PORT ); else await app.listen( 3000 );
+  await app.listen( process.env.PORT );
 }
 bootstrap();
