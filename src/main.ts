9b41bf7676074ce3ddad687afe565ea9f10bfa1d
lionmanbloodsucker <lionmanbloodsucker@yahoo.com>
2019-06-10 17:51:10 +0700

    add validation pipes and init firebase

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as firebase_admin from 'firebase-admin';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen( process.env.PORT );
+	firebase_admin.initializeApp({
+		credential: firebase_admin.credential.cert( require('../sassets/kuis-dharma-firebase-adminsdk-1j5uv-dda1db7735.json') ),
+		storageBucket: process.env.STORAGE_BUCKET_URL,
+	});
+
+	const app = await NestFactory.create(AppModule);
+
+	app.useGlobalPipes( new ValidationPipe() );
+
+	await app.listen( process.env.PORT );
 }
 bootstrap();
